8e12e76795c7715a7f096896c31653355bcc8840
Prathik Reddy <tvkp333555@gmail.com>
2021-12-25 13:28:55 +0530

    added sorting ,paginatoin,delete funtionality

--- src/Components/Favourites.js
+++ src/Components/Favourites.js
@@ -10,6 +10,7 @@ export default class Favourites extends Component {
             movies:[],
             currTxt : "",
             limit : 5,
+            currPage:1,
         }
     }
     componentDidMount(){
@@ -35,6 +36,55 @@ export default class Favourites extends Component {
             currgen:genre
         })
     }
+    sortPopAsc=()=>{
+        let temp = this.state.movies;
+        temp.sort(function(a,b){
+            return a.popularity-b.popularity;
+        })
+        this.setState({
+            movies:[...temp]
+        })
+    }
+    sortPopDesc=()=>{
+        let temp = this.state.movies;
+        temp.sort(function(a,b){
+            return b.popularity-a.popularity;
+        })
+        this.setState({
+            movies:[...temp]
+        })
+    }
+    sortRatingAsc=()=>{
+        let temp = this.state.movies;
+        temp.sort(function(a,b){
+            return a.vote_average-b.vote_average;
+        })
+        this.setState({
+            movies:[...temp]
+        })
+    }
+    sortRatingDesc=()=>{
+        let temp = this.state.movies;
+        temp.sort(function(a,b){
+            return b.vote_average-a.vote_average;
+        })
+        this.setState({
+            movies:[...temp]
+        })
+    }
+    handlePageChange=(page)=>{
+        this.setState({
+            currPage:page,
+        })
+    }
+    handleDelete=(id)=>{
+        let newarr = [];
+        newarr = this.state.movies.filter((movieObj)=>movieObj.id!=id)
+        this.setState({
+            movies:[...newarr]
+        })
+        localStorage.setItem("movies",JSON.stringify(newarr))
+    }
     render() {
         let genreids = {28:'Action',12:'Adventure',16:'Animation',35:'Comedy',80:'Crime',99:'Documentary',18:'Drama',10751:'Family',14:'Fantasy',36:'History',
         27:'Horror',10402:'Music',9648:'Mystery',10749:'Romance',878:'Sci-Fi',10770:'TV',53:'Thriller',10752:'War',37:'Western'};
@@ -51,10 +101,19 @@ export default class Favourites extends Component {
         if(this.state.currgen!="All Genres"){
             filterArray = this.state.movies.filter((movieObj)=>genreids[movieObj.genre_ids[0]]==this.state.currgen)
         }
+        let pages = Math.ceil(filterArray.length / this.state.limit);
+        let pagesArr = [];
+        for(let i = 1; i <= pages; i++){
+            pagesArr.push(i);
+        }
+        console.log(pagesArr);
+        let si = (this.state.currPage-1)*this.state.limit;
+        let ei = si+this.state.limit;
+        filterArray = filterArray.slice(si,ei);
         return (
             <div className="main">
-                <div className="row">
-                    <div className="col-3">
+                <div className="row mx-2">
+                    <div className="col-md-3 col-sm-12">
                         <ul className="list-group favourites-genres">
                         {
                             this.state.genres.map((genre) => (
@@ -66,19 +125,19 @@ export default class Favourites extends Component {
 
                         </ul>
                     </div>
-                    <div className="col-9 favourites-table">
+                    <div className="col-md-9 favourites-table col-sm-12">
                         <div className="row">
                             <input type="text" className="input-group-text col"placeholder="Search" value={this.state.currTxt} onChange={(e)=>this.setState({currTxt:e.target.value})}/>
-                            <input type="number" className="input-group-text col" placeholder="Rows Count" />
+                            <input type="number" className="input-group-text col" placeholder="Rows Count" value={this.state.limit} onChange={(e)=>this.setState({limit:e.target.value})}/>
                         </div>
-                        <div className="row">
+                        <div className="row mx-2">
                             <table class="table">
                                 <thead>
                                     <tr>
                                     <th scope="col">Title</th>
                                     <th scope="col">Genre</th>
-                                    <th scope="col"><i class="fas fa-sort-up"></i> Popularity <i class="fas fa-sort-down"></i></th>
-                                    <th scope="col"><i class="fas fa-sort-up"></i> Rating <i class="fas fa-sort-down"></i></th>
+                                    <th scope="col"><i class="fas fa-sort-up" onClick={this.sortPopAsc}></i> Popularity <i class="fas fa-sort-down"  onClick={this.sortPopDesc}></i></th>
+                                    <th scope="col"><i class="fas fa-sort-up"  onClick={this.sortRatingAsc}></i> Rating <i class="fas fa-sort-down"  onClick={this.sortRatingDesc}></i></th>
                                     <th scope="col">Remove</th>
                                     </tr>
                                 </thead>
@@ -102,9 +161,11 @@ export default class Favourites extends Component {
                             <nav aria-label="Page navigation example">
                                 <ul class="pagination">
                                     <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                    {
+                                            pagesArr.map((page)=>(
+                                                <li class="page-item"><a class="page-link" onClick={()=>this.handlePageChange(page)}>{page}</a></li>
+                                            ))
+                                        }
                                     <li class="page-item"><a class="page-link" href="#">Next</a></li>
                                 </ul>
                             </nav>
